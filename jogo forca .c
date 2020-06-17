7a3c20ec8f7cc2ad857d3b8a7f779e32b0a971ed
fernandaguimaraes <56983679+fernandaguimaraes@users.noreply.github.com>
2020-06-17 18:29:15 -0300

    Add files via upload

---
+++ jogo forca .c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<locale.h>
+//-----------------------------------------------------------
+void inicio()
+{
+	printf("|*************************************|");
+	printf("|***SEJA BEM VINDO AO JOGO DA FORCA***|");
+	printf("|*************************************|");	
+}
+
+//-----------------------------------------------------------
+
+int main()
+{
+	char palavra1[] = {'f','u','l','a','n','o'};
+	char palavra2[] = {'a','b','a','c','a','t','e'};
+	char palavra3[] = {'c','o','e','l','h','o'};
+	char palavra4[] = {'s','a','n','d','á','l','i','a'};
+	char palavra5[] = {'p','r','a','i','a'};
+	
+	printf ("Escolha uma das cinco palavras disponiveis digitando um numero de 1 a 5:");
+	
+}
+
+
+
